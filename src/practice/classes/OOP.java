1d0f0e359da1a65e0979474ffd72fbe438333ee9
PlayGround+ <lemarieannetoinette@gmail.com>
2026-04-09 00:35:25 +0800

    Update OOP.java

--- src/practice/classes/OOP.java
+++ src/practice/classes/OOP.java
@@ -175,7 +175,6 @@ class Unit implements AttackBehavior, MovementBehavior {
     public void printUnitInfo() {
         System.out.printf("Unitname: %s | Health: %d | Mana: %d | Attack Range: %d | Movement Speed: %d\n", name, health, mana, attackRange, moveSpeed);
     }
-
 }
 
 class Spell {
@@ -229,9 +228,7 @@ class OOP {
         System.out.println("--- Starting WC3 Sandbox ---");
 
         Hero dreadlord = new Hero("Mal'Ganis", 1200, 400, 150, 290, AttackType.Melee, MoveType.Foot, 22, 15, 18 );
-
         Unit footman = new Unit("Footman", 420, 0, 100, 270, AttackType.Melee, MoveType.Foot);
-
         Spell deathCoil = new Spell("Death Coil", 100, 200);
 
         System.out.println("\n---Action: Unit Info---");
