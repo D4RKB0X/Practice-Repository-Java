8967c636193ff18d2e03e069fbecdfc9a201a3ec
Lain Iwakura <kobeangelovillegas12@gmail.com>
2026-04-09 11:45:52 +0800

    V2.5 Update

--- src/practice/classes/OOP.java
+++ src/practice/classes/OOP.java
@@ -240,6 +240,7 @@ class Hero extends Unit {
             return;
         }
         else {
+            heroLevel += levelGained;
             strength += ADD_STRENGTH * levelGained;
             agility += ADD_AGILITY * levelGained;
             intelligence += ADD_INTELLIGENCE * levelGained;
@@ -253,13 +254,67 @@ class Hero extends Unit {
     }
 }
 
-class Main {
-    public static void main(String[] args) {
-        System.out.println("--- Starting WC3 Sandbox ---");
+class Army {
+    private final String name;
+    private final List<Unit> armyUnits;
+    private static final int MAX_UNITS_IN_ARMY = 12;
 
-        Hero dreadlord = new Hero("Mal'Ganis", 1200, 400, 150, 290, AttackType.Melee, MoveType.Foot, 22, 15, 18);
-        Unit footman = new Unit("Footman", 420, 0, 100, 270, AttackType.Melee, MoveType.Foot);
-        Unit peasant = new Unit("Peasant", 250, 0, 10, 220, AttackType.None, MoveType.Foot);
+    public Army(String name) {
+        this.name = name;
+        this.armyUnits = new ArrayList<Unit>(MAX_UNITS_IN_ARMY);
+    }
+
+    public String getArmyName() { return name; }
+    public int getArmySize() { return armyUnits.size(); }
+    public boolean isEmpty() { return armyUnits.isEmpty(); }
+
+    public void addUnit(Unit inputUnit) {
+        if(inputUnit == null) {
+            System.err.print("Cannot Add Null!\n");
+            return;
+        }
+        else if (armyUnits.size() >= MAX_UNITS_IN_ARMY) {
+            System.err.print("Army Full Capacity!\n");
+            return;
+        }
+        else {
+            armyUnits.add(inputUnit);
+            System.out.printf("%s has been added to the battlefield!\n", inputUnit.getUnitName());
+        }
+    }
+
+    public void removeUnit(Unit inputUnit) {
+        if(!armyUnits.contains(inputUnit)) {
+            System.err.printf("%s is not in %s!\n", inputUnit.getUnitName(), name);
+            return;
+        }
+        else {
+            armyUnits.remove(inputUnit);
+            System.out.printf("%s has been removed from %s!\n", inputUnit.getUnitName(), name);
+        }
+    }
+
+    public void printArmyInfo() {
+        System.out.printf("Army: %s | Units: %d/%d\n", name, armyUnits.size(), MAX_UNITS_IN_ARMY);
+        for(Unit u : armyUnits) {
+            u.printUnitInfo();
+        }
+    }
+}
+
+class OOP {
+    public static void main(String[] args) {
+        System.out.println("=== Starting WC3 Sandbox ===");
+
+        Hero dreadlord   = new Hero("Mal'Ganis", 1200, 400, 150, 290, AttackType.Melee, MoveType.Foot, 22, 15, 18);
+        Unit ghoul       = new Unit("Ghoul", 340, 0, 100, 250, AttackType.Melee, MoveType.Foot);
+        Unit abomination = new Unit("Abomination", 1100, 0, 110, 190, AttackType.Melee, MoveType.Foot);
+        Unit gargoyle    = new Unit("Gargoyle", 500, 0, 300, 400, AttackType.Ranged, MoveType.Fly);
+
+        Unit footman  = new Unit("Footman", 420, 0, 100, 270, AttackType.Melee, MoveType.Foot);
+        Unit peasant  = new Unit("Peasant", 250, 0, 10, 220, AttackType.None, MoveType.Foot);
+        Unit archer   = new Unit("Archer", 280, 0, 450, 300, AttackType.Ranged, MoveType.Foot);
+        Unit knight   = new Unit("Knight", 825, 0, 100, 270, AttackType.Melee, MoveType.Horse);
 
         Spell deathCoil    = new Spell("Death Coil", 100, 200);
         Spell carrionSwarm = new Spell("Carrion Swarm", 125, 300);
@@ -267,52 +322,82 @@ class Main {
         Spell inferno      = new Spell("Inferno", 175, 100);
         Spell dummy        = new Spell("Dummy Spell", 50, 10);
 
-        System.out.println("\n--- Unit Info ---");
-        dreadlord.printUnitInfo();
-        footman.printUnitInfo();
-        peasant.printUnitInfo();
+        Army scourge  = new Army("Scourge");
+        Army alliance = new Army("Alliance");
 
-        System.out.println("\n--- Animations ---");
+        System.out.println("\n=== Building Armies ===");
+        scourge.addUnit(dreadlord);
+        scourge.addUnit(ghoul);
+        scourge.addUnit(abomination);
+        scourge.addUnit(gargoyle);
+
+        alliance.addUnit(footman);
+        alliance.addUnit(peasant);
+        alliance.addUnit(archer);
+        alliance.addUnit(knight);
+
+        System.out.println("\n=== Army Rosters ===");
+        scourge.printArmyInfo();
+        System.out.println();
+        alliance.printArmyInfo();
+
+        System.out.println("\n=== Animations ===");
         dreadlord.attackAnimation();
         dreadlord.movementAnimation();
+        gargoyle.attackAnimation();
+        gargoyle.movementAnimation();
+        knight.movementAnimation();
         peasant.attackAnimation();
 
-        System.out.println("\n--- Level Up ---");
-        dreadlord.heroLevelUp(1);
+        System.out.println("\n=== Mal'Ganis Levels Up ===");
+        dreadlord.heroLevelUp(3);
+        dreadlord.heroLevelUp(9);
+        dreadlord.heroLevelUp(0);
 
-        System.out.println("\n--- Adding Spells ---");
+        System.out.println("\n=== Learning Spells ===");
         dreadlord.unitAddSpell(deathCoil);
         dreadlord.unitAddSpell(carrionSwarm);
         dreadlord.unitAddSpell(sleep);
         dreadlord.unitAddSpell(inferno);
         dreadlord.unitAddSpell(dummy);
 
-        System.out.println("\n--- Spell Info ---");
+        System.out.println("\n=== Spell Info ===");
         deathCoil.printSpellInfo();
         carrionSwarm.printSpellInfo();
+        sleep.printSpellInfo();
+        inferno.printSpellInfo();
 
-        System.out.println("\n--- Casting Spells ---");
+        System.out.println("\n=== The Battle Begins ===");
         dreadlord.unitUseSpell(deathCoil, footman);
         dreadlord.unitUseSpell(carrionSwarm, footman);
-        dreadlord.unitUseSpell(sleep, peasant);
-
-        System.out.println("\n--- Cast Unowned Spell ---");
-        footman.unitUseSpell(deathCoil, peasant);
-
-        System.out.println("\n--- Not Enough Mana ---");
+        dreadlord.unitUseSpell(sleep, knight);
         dreadlord.unitUseSpell(inferno, peasant);
 
-        System.out.println("\n--- Remove Spell ---");
+        System.out.println("\n=== Unowned Spell Attempt ===");
+        footman.unitUseSpell(deathCoil, peasant);
+
+        System.out.println("\n=== Spell Removal ===");
         dreadlord.unitRemoveSpell(sleep);
         dreadlord.unitRemoveSpell(dummy);
 
-        System.out.println("\n--- Final Status ---");
-        dreadlord.printUnitInfo();
-        footman.printUnitInfo();
-        peasant.printUnitInfo();
-        System.out.printf("Footman alive: %b\n", footman.isAlive());
-        System.out.printf("Peasant alive: %b\n", peasant.isAlive());
+        System.out.println("\n=== Army Edge Cases ===");
+        scourge.addUnit(null);
+        alliance.removeUnit(dreadlord);
 
-        System.out.println("\n--- Sandbox Complete ---");
+        System.out.println("\n=== Updating Alliance Roster ===");
+        alliance.removeUnit(footman);
+        alliance.printArmyInfo();
+
+        System.out.println("\n=== Final Status ===");
+        scourge.printArmyInfo();
+        System.out.println();
+        alliance.printArmyInfo();
+        System.out.println();
+        System.out.printf("Footman alive: %b\n", footman.isAlive());
+        System.out.printf("Knight alive: %b\n", knight.isAlive());
+        System.out.printf("Peasant alive: %b\n", peasant.isAlive());
+        System.out.printf("Mal'Ganis alive: %b\n", dreadlord.isAlive());
+
+        System.out.println("\n=== The Scourge Prevails. Sandbox Complete. ===");
     }
 }
